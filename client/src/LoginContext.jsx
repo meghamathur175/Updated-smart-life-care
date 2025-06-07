5e7c1026c31bc8374919e9b9e230058d3eae4b1c
meghamathur175 <meghamathur175@gmail.com>
2025-06-07 14:31:46 +0530

    loginContext

---
+++ client/src/LoginContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const LoginContext = createContext();
+
+export const LoginProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(() => {
+    return localStorage.getItem("isLoggedIn") === "true";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("isLoggedIn", isLoggedIn);
+  }, [isLoggedIn]);
+
+  return (
+    <LoginContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
+      {children}
+    </LoginContext.Provider>
+  );
+};
+
+export const useLogin = () => useContext(LoginContext);
