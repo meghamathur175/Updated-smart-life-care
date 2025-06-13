6b5daf553e81a883cef68df0d28584d1c08d4f88
meghamathur175 <meghamathur175@gmail.com>
2025-06-13 16:00:53 +0530

    server

---
+++ server/config/routes/driverRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  forgotPasswordDriver,
+  registerDriver,
+  loginDriver,
+  logoutDriver,
+  resetPasswordDriver,
+  getAllDrivers,
+  getDriversByPartner,
+} = require("../controllers/driverAuthController");
+
+router.post("/register", registerDriver);
+router.post("/login", loginDriver);
+router.post("/forgot-password", forgotPasswordDriver);
+router.post("/reset-password/:token", resetPasswordDriver);
+router.get("/logout", logoutDriver);
+router.get("/all", getAllDrivers);
+router.get("/by-partner", getDriversByPartner);
+
+module.exports = router;
---
+++ server/config/routes/partnerDriverRoutes.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const partnerDriverController = require("../controllers/partnerDriverController");
+
+router.get("/", partnerDriverController.getAllPartnerDrivers);
+router.post("/", partnerDriverController.addPartnerDriver);
+router.put("/:id", partnerDriverController.updatePartnerDriver);
+router.delete("/:id", partnerDriverController.deletePartnerDriver);
+
+module.exports = router;
