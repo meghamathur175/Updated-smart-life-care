a0a9dfab1f9a97f3313fa9784be49d597bc4c250
meghamathur175 <meghamathur175@gmail.com>
2025-06-14 14:09:14 +0530

    Delete server/config directory

--- server/config/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { processPayment, getKey } = require('../controllers/paymentController');
-
-router.route("/payment/process").post(processPayment);
-router.route("/getKey").get(getKey);
-
-module.exports = router;
