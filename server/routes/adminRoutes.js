2793edfed640ea321a5cf4300a10b79ca3af9090
meghamathur175 <meghamathur175@gmail.com>
2025-06-19 14:17:51 +0530

    Delete server directory

--- server/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const {
-  getAdminReports,
-} = require("../controllers/adminController");
-
-router.get("/admin-reports", getAdminReports);
-
-module.exports = router;
