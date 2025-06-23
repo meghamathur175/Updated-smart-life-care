c4e73ac79e39f284984b270313fd7f75952dbcce
meghamathur175 <meghamathur175@gmail.com>
2025-06-23 19:10:26 +0530

    Delete server directory

--- server/routes/getRequestStatusByBookingIdRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getRequestStatusByBookingId } = require("../controllers/getRequestStatusByBookingId");
-
-router.get("/request-status/booking-id/:bookingId", getRequestStatusByBookingId);
-
-module.exports = router;
