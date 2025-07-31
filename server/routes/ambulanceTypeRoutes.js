d762fdd65091d0d1ec0d48b7003ca14def480c47
meghamathur175 <meghamathur175@gmail.com>
2025-07-31 15:14:48 +0530

    Delete server directory

--- server/routes/ambulanceTypeRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAmbulanceTypes } = require("../controllers/ambulanceTypeController");
-
-router.get("/ambulance-types", getAmbulanceTypes);
-
-module.exports = router;
